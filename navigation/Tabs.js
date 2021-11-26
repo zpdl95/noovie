afb2a45f62baea0ee1d8b0c25ff58745e1ccb151
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-11-26 21:19:47 +0900

    #2.7 Dark Mode

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,16 +3,37 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import TV from "../screens/TV";
 import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator screenOptions={{ tabBarLabelStyle: { color: "tomato" } }}>
-    <Tab.Screen name="Movies" component={Movies} options={{ color: "blue" }} />
-    <Tab.Screen name="TV" component={TV} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  /* 디바이스의 dark mode light mode를 감지하는 hooks다 */
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: isDark ? "grey" : "white" },
+        tabBarActiveTintColor: isDark ? "white" : "grey",
+        tabBarInactiveTintColor: isDark ? "grey" : "white",
+        headerStyle: {
+          backgroundColor: isDark ? "grey" : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : "grey",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{ color: "blue" }}
+      />
+      <Tab.Screen name="TV" component={TV} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 export default Tabs;
 
 /* screenOptions는 모든 스크린에 동일하게 적용된다.
