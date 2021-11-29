3bf5f3da2ed43bce9ecf5a904de20a048ca071f8
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-11-30 01:50:22 +0900

    #3.2 Movies Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View, Text, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { Dimensions } from "react-native";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => null;
+const API_KEY = "9db5794f039ec550f11e71750544ca9e";
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+/* 디바이스의 창의 크기를 가져오는 api */
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "yellow" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 export default Movies;
