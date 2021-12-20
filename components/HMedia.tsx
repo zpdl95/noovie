c00bfaa775077b1836a7ab5ec8dc85d4a127f96a
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-20 22:45:24 +0900

    #3.21 Detail Screen part One

--- components/HMedia.tsx
+++ components/HMedia.tsx
@@ -57,7 +57,11 @@ const HMedia: React.FC<HMediaProps> = ({
     /* navigate는 다른 screen으로 이동해주는 함수
     첫번째인자로 screen이름을 넣으면 같은 Tab or Stack Navigator안에서 이동
     다른 Navigator에 있는 screen으로 이동하려면 ↓ 처럼 작성 */
-    navigation.navigate("Stack", { screen: "Detail" });
+    /* navigate로 이동할때 screen name과 params에 값을 전달한다 */
+    navigation.navigate("Stack", {
+      screen: "Detail",
+      params: { originalTitle },
+    });
   };
   return (
     <TouchableOpacity onPress={goToDetail}>
--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Detail from "../screens/Detail";
+import { useColorScheme } from "react-native";
 
 const NativeStack = createNativeStackNavigator();
 
 /* StackNavigator에 있는 navigation prop의 사용 방법
 'const ScreenOne = ({ navigation: { navigate } }) =>'
 모든 Screen컴포넌트에 들어간 component는 navigation prop를 가진다 */
-const Stack = () => (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name="Detail" component={Detail} />
-  </NativeStack.Navigator>
-);
+const Stack = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: isDark ? "#1e272e" : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "#ffa801" : "#1e272e",
+        },
+      }}
+    >
+      <NativeStack.Screen name="Detail" component={Detail} />
+    </NativeStack.Navigator>
+  );
+};
 export default Stack;
