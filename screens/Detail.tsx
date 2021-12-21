53d2d9e63612aacc39e67fc82cdacbd806221b0f
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-21 18:14:38 +0900

    #3.23 Detail Screen part Three

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,12 +1,42 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Text } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
 import { Movie, TV } from "../api";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  width: 70%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 30px;
+  padding: 0 20px;
 `;
 
 /* Root Navigator가 갖고 있는 Screen들의 type을 만들어야한다 */
@@ -27,15 +57,30 @@ const Detail: React.FC<DetailScreenProps> = ({
   /* component가 didmount됐을 때 실행시킴  */
   useEffect(() => {
     setOptions({
-      title:
-        "original_title" in params
-          ? params.original_title
-          : params.original_name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          colors={["transparent", "#1e272e"]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
